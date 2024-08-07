1c9e3644e76e5281b6d0499ec5f94348edbef1f5
AdventureLouis <okezten@gmail.com>
2024-08-07 18:10:31 +0800

    BeforeInstallHook updated in appspec.yml file

--- scripts/remove_dir.sh
+++ scripts/remove_dir.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-rm -r /var/www/html/*
+
+# This ccommand will remove  all text files
+# rm -r /var/www/html/*
+
+# This command will ignore .gitignore file
+rsync -av --exclude='.gitignore' /source/ /destination/
